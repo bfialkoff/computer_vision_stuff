667a30819ceb50b2fc1018e1518b13589f624512
bfialkoff <39707315+bfialkoff@users.noreply.github.com>
2019-07-09 10:07:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='cv_stuff',
+   name='computer_vision_stuff',
    version='1.0',
    description='A useful module',
    author='B F',
    author_email='foomail@foo.com',
-   packages=['cv_stuff'],  #same as name
+   packages=['computer_vision_stuff'],  #same as name
    )
