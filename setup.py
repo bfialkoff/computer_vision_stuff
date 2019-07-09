bcfeeab72cdbcf7e372b27f59be5a681365a4255
bfialkoff <39707315+bfialkoff@users.noreply.github.com>
2019-07-09 10:06:12 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='cv_stuff',
+   version='1.0',
+   description='A useful module',
+   author='B F',
+   author_email='foomail@foo.com',
+   packages=['cv_stuff'],  #same as name
+   )
